a028e14f46eebb418c26d680bb2d46c4d1847a1c
FILIP <siposlaw95@gmail.com>
2022-06-03 12:14:39 +0200

    f

--- functions.js
+++ functions.js
@@ -23,6 +23,42 @@
     let food = ['apple', 'watermelon', 'bannana', 'pear', 'carrot', 'beetroot', 'beans'];
     let bigger = food.filter(isBigger);
     let startsb = food.filter(startsB);
+    console.log(food);
     console.log(bigger);
     console.log(startsb);
+}
+{
+    function isEven(num) {
+        return (num % 2 == 0);
+    }
+    function isOdd(num) {
+        return (num % 2 != 0);
+    }
+    let number = [2, 4, 7, 11, 14, 19, 21, 100];
+    let even = number.filter(isEven);
+    let odd = number.filter(isOdd);
+    console.log(number);
+    console.log(even);
+    console.log(odd);
+}
+{
+    let food = ['Banana', 'Apple', 'Macaronni', 'Pasta', 'Pizza'];
+    function correct_food(str) {
+        return (str.length > 5 && str.charAt(1) == 'a');
+    }
+    let correct = food.filter(correct_food);
+    console.log(food);
+    console.log(correct);
+}
+{
+    const suma = function (a, b) {
+        console.log(`wypisane ${a} oraz ${b}`);
+    };
+    console.log(suma(2));
+}
+{
+    function guest(name, ...data) {
+        return `Welcome, ${name} another yours data: ${data.join(", ")}`;
+    }
+    console.log(guest("Cezary", "Opole", "cezary@wp.pl"));
 }
